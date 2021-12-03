a0079f9e3c7a62601e09ea85e636df36b1f2732a
“Allen” <alnwangchi@gmail.com>
2021-12-03 14:32:59 +0800

    使用 useEffect 及啟一個 json-server 自己 call

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { API_GET_DATA } from '../../global/constants';
 
 import Form from "./components/Form";
 import List from "./components/List";
@@ -6,6 +7,30 @@ import "./index.css";
 
 const Home = () => {
   const [data, setData] = useState([]); // state 改變 react 就會重新選渲染，底下兩個元件共用此狀態
+
+  //
+  async function fetchData() { // 包裝 fetch 回來的 Promise 物件
+    const res = await fetch(API_GET_DATA)
+    const data = await res.json()
+    console.log(data);
+  }
+  
+  useEffect(() => { // reload都會先執行一次
+    fetch(API_GET_DATA)
+    .then(res => res.json())
+    .then((data) => {
+      console.log(data);
+    })
+  },[data])
+
+  /*
+  useEffect(() => { // reload都會先執行一次
+    // 綁定的內容寫在這
+    return () => {
+      // 取消綁定的內容寫在這
+    }
+  },[data])
+  */
 
   return <div className="app">
     <Form addData={setData} name="Allen" />
