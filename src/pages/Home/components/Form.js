0b0fb0aacb7808c2054291a5cbc9bb57397aa959
“Allen” <alnwangchi@gmail.com>
2021-12-07 14:17:18 +0800

    使用 dayjs 新增取得當前時間的按鈕

--- src/pages/Home/components/Form.js
+++ src/pages/Home/components/Form.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { v4 } from "uuid"; // 產生唯一值的 lib
+import dayjs from "dayjs"
 
 const Form = ({addData, submitState}) => {
 
@@ -14,6 +15,7 @@ const Form = ({addData, submitState}) => {
   const [time, setTime] = useState("");
   const timeTyping = (e) => {
     setTime(e.target.value);
+    console.log(e.target.value);
   };
   // console.log(note, date, time);
   // const [formData, setFormData] = useState({ note: "", time: "", date: "" });
@@ -39,21 +41,32 @@ const Form = ({addData, submitState}) => {
 
   const enterAdd = (e) => {
     if (e.keyCode === 13) {
-      console.log('Enter!');
       addItem()
     }
   }
+  // 自動取時功能按鈕
+  const selectNow = () => {
+    const nowDate = dayjs().format('YYYY-MM-DD') // 拿到當前日期
+    const nowTime = dayjs().format('HH:mm') // 拿到當前時間
+    setDate(nowDate)
+    setTime(nowTime)
+  }
+  
+
 
   return (
     <div>
       <h1>Allen TODO</h1>
       <p>事項:</p>
-      <input type="text" value={note} onChange={noteTyping} onKeyDown={(e) => enterAdd(e) }/>
+      <input type="text" value={note} onChange={noteTyping} onKeyDown={enterAdd}/>
       <div className="input_time">
-        <span>日期</span>
-        <input type="date" value={date} onChange={dateTyping} />
-        <span>時間:</span>
-        <input type="time" value={time} onChange={timeTyping} />
+        <label>日期
+          <input type="date" value={date} onChange={dateTyping} />
+        </label>
+        <label>時間:
+          <input type="time" value={time} onChange={timeTyping} />
+        </label>
+        <button className="now_btn" onClick={selectNow}>JUST NOW</button>
       </div>
 
 
